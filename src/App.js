2c536dc778f24675b43b624f5cf8aba560614ae7
IA-Lopez <ialopez.contacto@gmail.com>
2024-06-08 21:25:54 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-setIntervalIdimport React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Modal from 'react-modal';
 import './App.css';
