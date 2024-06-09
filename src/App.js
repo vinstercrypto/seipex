6e095b96e5e44cc0cfb7280899333077941c60c1
IA-Lopez <ialopez.contacto@gmail.com>
2024-06-09 03:46:24 +0200

    fk

--- src/App.js
+++ src/App.js
@@ -181,7 +181,7 @@ const App = () => {
               </div>
               <div>
                 <label>Sell Percentage (%):</label>
-                <input type="number" value={ca} onChange={(e) => setSellPercentConfig(e.target.value)} />
+                <input type="number" value={sellPercentConfig} onChange={(e) => setSellPercentConfig(e.target.value)} />
               </div>
               {intervalId ? (
                 <button onClick={stopChecking}>Stop Auto-Sell</button>
@@ -206,7 +206,7 @@ const App = () => {
                 <label>Sell Percentage (%):</label>
                 <input type="number" value={sellPercentConfig} onChange={(e) => setSellPercentConfig(e.target.value)} />
               </div>
-              <button onClick={() => sellPercent(sellPercentConfig)}>Sell Percentage</button>
+              <button onClick={() => sellPercent(ca)}>Sell Percentage</button>
             </div>
           )}
 
