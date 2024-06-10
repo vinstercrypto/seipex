e61931e1001191360384ffb7122525ce098c73c4
IA-Lopez <ialopez.contacto@gmail.com>
2024-06-10 14:03:16 +0200

    v0.1
    Better logs
    Console goes down
    Fix collision between auto-fetch and auto-sell

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import Modal from 'react-modal';
 import './App.css';
@@ -18,11 +18,19 @@ const App = () => {
   const [modalMessage, setModalMessage] = useState('');
   const [calledTokens, setCalledTokens] = useState(new Set());
   const [consoleMessages, setConsoleMessages] = useState([]);
+  const consoleEndRef = useRef(null); // Añadir ref
+
+  const logAndUpdateConsole = (message) => {
+    console.log(message);
+    updateConsole(message);
+  };
 
   const fetchData = async () => {
+    logAndUpdateConsole('Fetching data');
     if (!wallet || !input) {
       setModalMessage('Please fill in Wallet Address and Input.');
       setModalIsOpen(true);
+      logAndUpdateConsole('Fetching data aborted: Wallet Address and Input required');
       return;
     }
 
@@ -35,17 +43,19 @@ const App = () => {
       });
       const results = response.data.results;
       setData(results);
+      logAndUpdateConsole('Fetching data completed successfully');
       return results;
     } catch (error) {
-      console.error("Error fetching data:", error);
-      updateConsole(`Error fetching data: ${error.message}`);
+      logAndUpdateConsole(`Error fetching data: ${error.message}`);
     }
   };
 
   const sellPercent = async (address) => {
+    logAndUpdateConsole(`Starting sell for CA: ${address}`);
     if (!pk || !ca || !sellPercentConfig) {
       setModalMessage('Please fill in Private Key, Contract Address, and Sell Percentage.');
       setModalIsOpen(true);
+      logAndUpdateConsole('Sell aborted: Missing required fields');
       return;
     }
 
@@ -57,78 +67,94 @@ const App = () => {
           percent: sellPercentConfig
         }
       });
-      console.log(`Sell of ${sellPercentConfig}% called for contract address: ${ca}`);
-      updateConsole(`Sell of ${sellPercentConfig}% called for contract address: ${ca}`);
+      logAndUpdateConsole(`Sell of ${sellPercentConfig}% called for CA: ${address}`);
     } catch (error) {
-      console.error(`Error calling sell endpoint for ${ca}:`, error);
-      updateConsole(`Error calling sell endpoint for ${ca}: ${error.message}`);
+      logAndUpdateConsole(`Error calling sell endpoint for ${address}: ${error.message}`);
     }
   };
 
-  const startChecking = async () => {
+  const startAutoSell = async () => {
+    logAndUpdateConsole('Starting Auto-sell');
     if (!wallet || !input || !pk || !ca || !roiThreshold || !sellPercentConfig) {
       setModalMessage('Please fill in all the fields for Auto-Sell.');
       setModalIsOpen(true);
+      logAndUpdateConsole('Auto-sell aborted: Missing required fields');
       return;
+    }
+
+    if (fetchIntervalId) {
+      clearInterval(fetchIntervalId);
+      setFetchIntervalId(null);
+      logAndUpdateConsole('Stopped fetching to avoid conflict with auto-sell');
     }
 
     setModalMessage('This action will start a process that checks and sells automatically. Keep the page open.');
     setModalIsOpen(true);
 
-    console.log('Starting auto-sell process');
-    updateConsole('Starting auto-sell process');
+    logAndUpdateConsole('Starting auto-sell process');
 
     const id = setInterval(async () => {
-      console.log('Fetching data');
-      updateConsole('Fetching data');
+      logAndUpdateConsole('Fetching data for auto-sell');
       const data = await fetchData();
-      console.log('Starting autosell check');
-      updateConsole('Starting autosell check');
-      for (const result of data) {
-        const { address, ROI, symbol } = result;
-        if (ca && ca !== '0x' && address === ca) {
-          if (parseFloat(ROI) >= roiThreshold && !calledTokens.has(address)) {
-            console.log("Checking ROI for selected CA: " + address);
-            updateConsole("Checking ROI for selected CA: " + address);
-            setCalledTokens(prev => new Set(prev.add(address)));
-            await sellPercent(address);
-          } else {
-            console.log(roiThreshold + "% ROI not reached (" + ROI + ")");
-            updateConsole(roiThreshold + "% ROI not reached (" + ROI + ")");
-          }
-        } else if (ca === '0x') {
-          if (parseFloat(ROI) >= roiThreshold && !calledTokens.has(address)) {
-            console.log("Checking ROI for CA: " + address);
-            updateConsole("Checking ROI for CA: " + address);
-            setCalledTokens(prev => new Set(prev.add(address)));
-            await sellPercent(address);
-          } else {
-            console.log(roiThreshold + "% ROI not reached (" + ROI + ") for " + symbol);
-            updateConsole(roiThreshold + "% ROI not reached (" + ROI + ") for " + symbol);
+      if (data) {
+        logAndUpdateConsole('Starting autosell');
+        for (const result of data) {
+          const { address, ROI, symbol } = result;
+          if (ca && ca !== '0x' && address === ca) {
+            if (parseFloat(ROI) >= roiThreshold && !calledTokens.has(address)) {
+              logAndUpdateConsole(`Checking ROI for selected CA: ${address}`);
+              setCalledTokens(prev => new Set(prev.add(address)));
+              await sellPercent(address);
+            } else {
+              logAndUpdateConsole(`${roiThreshold}% ROI not reached (${ROI})`);
+            }
+          } else if (ca === '0x') {
+            if (parseFloat(ROI) >= roiThreshold && !calledTokens.has(address)) {
+              logAndUpdateConsole(`Checking ROI for CA: ${address}`);
+              setCalledTokens(prev => new Set(prev.add(address)));
+              await sellPercent(address);
+            } else {
+              logAndUpdateConsole(`${roiThreshold}% ROI not reached (${ROI}) for ${symbol}`);
+            }
           }
         }
+      } else {
+        logAndUpdateConsole('No data fetched for autosell');
       }
     }, 3000); // Refresh every 3 seconds
+
     setIntervalId(id);
   };
 
-  const stopChecking = () => {
+  const stopAutoSell = () => {
+    logAndUpdateConsole('Stopping auto-sell process');
     if (intervalId) {
       clearInterval(intervalId);
       setIntervalId(null);
       setModalMessage('Auto-Sell stopped.');
       setModalIsOpen(true);
-      updateConsole('Auto-Sell stopped.');
+      logAndUpdateConsole('Auto-Sell stopped.');
     }
   };
 
   const toggleFetching = () => {
+    logAndUpdateConsole('Toggling fetch process');
+    if (intervalId) {
+      setModalMessage('Auto-sell is already active and auto-fetching');
+      setModalIsOpen(true);
+      logAndUpdateConsole('Fetch process not started: Auto-sell is already active');
+      return;
+    }
+
     if (fetchIntervalId) {
       clearInterval(fetchIntervalId);
       setFetchIntervalId(null);
+      logAndUpdateConsole('Fetch process stopped');
     } else {
+      fetchData();
       const id = setInterval(fetchData, 3000); // Refresh every 3 seconds
       setFetchIntervalId(id);
+      logAndUpdateConsole('Fetch process started');
     }
   };
 
@@ -140,10 +166,16 @@ const App = () => {
     setConsoleMessages(prevMessages => [...prevMessages, message]);
   };
 
+  useEffect(() => {
+    if (consoleEndRef.current) {
+      consoleEndRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [consoleMessages]);
+
   return (
     <div className="container">
       <div className="header">
-        <h1>Seipex management</h1> 
+        <h1>Seipex Manager v0.1</h1> 
         <h3>by <a href="https://x.com/_IA_Lopez" target="_blank" rel="noopener noreferrer">@_IA_LOPEZ</a></h3> 
         <button className="copyButton" onClick={() => navigator.clipboard.writeText("0x742281DcbC8df500f1D5DF6B4269e65e72FcAef9")}>Copy TIP wallet</button> 
         <button className="copyButton" onClick={() => window.open(`https://github.com/IA-Lopez/seipex`, '_blank', 'noopener,noreferrer')}>Github</button>
@@ -184,9 +216,9 @@ const App = () => {
                 <input type="number" value={sellPercentConfig} onChange={(e) => setSellPercentConfig(e.target.value)} />
               </div>
               {intervalId ? (
-                <button onClick={stopChecking}>Stop Auto-Sell</button>
+                <button onClick={stopAutoSell}>Stop Auto-Sell</button>
               ) : (
-                <button onClick={startChecking}>Start Auto-Sell</button>
+                <button onClick={startAutoSell}>Start Auto-Sell</button>
               )}
             </div>
           )}
@@ -222,7 +254,7 @@ const App = () => {
                 <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
               </div>
               <button onClick={fetchData}>Fetch</button>
-              <button onClick={toggleFetching}>{fetchIntervalId ? 'Stop Fetching' : 'Start Fetching'}</button>
+              <button onClick={toggleFetching}>{fetchIntervalId ? 'Stop Auto-Fetching' : 'Start Auto-Fetching'}</button>
             </div>
           )}
         </div>
@@ -266,9 +298,8 @@ const App = () => {
                     <div className="fieldValue">{result.ROI}</div>
                   </div>
                   <div className="tokenField">
-                  <button className="copyButton" onClick={() => window.open(`https://basescan.org/address/${result.address}`, '_blank', 'noopener,noreferrer')}>Basescan</button>
-                  <button className="copyButton" onClick={() => window.open(`https://dexscreener.com/base/${result.address}`, '_blank', 'noopener,noreferrer')}>Dexscreener</button>
-     
+                    <button className="copyButton" onClick={() => window.open(`https://basescan.org/address/${result.address}`, '_blank', 'noopener,noreferrer')}>Basescan</button>
+                    <button className="copyButton" onClick={() => window.open(`https://dexscreener.com/base/${result.address}`, '_blank', 'noopener,noreferrer')}>Dexscreener</button>
                   </div>
                 </div>
               ))
@@ -282,6 +313,7 @@ const App = () => {
             {consoleMessages.map((msg, index) => (
               <div key={index} className="consoleMessage">{msg}</div>
             ))}
+            <div ref={consoleEndRef} />
           </div>
         </div>
       </div>
