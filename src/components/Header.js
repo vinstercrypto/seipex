84e173120f945212d50a8069905b3d2a765f8b85
IA-Lopez <ialopez.contacto@gmail.com>
2024-06-11 17:59:24 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Header = () => (
   <div className="header">
-    <h1>Seipex Manager v0.2</h1>
+    <h1>Seipex Manager v0.3</h1>
     <h3>by <a href="https://x.com/_IA_Lopez" target="_blank" rel="noopener noreferrer">@_IA_LOPEZ</a></h3>
     <button className="copyButton" onClick={() => navigator.clipboard.writeText("0x742281DcbC8df500f1D5DF6B4269e65e72FcAef9")}>Copy TIP wallet</button>
     <button className="copyButton" onClick={() => window.open(`https://github.com/IA-Lopez/seipex`, '_blank', 'noopener,noreferrer')}>Github</button>
